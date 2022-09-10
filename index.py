ecfd7fb36de247e576e20ca2a7c65b4543f0bba9
Mubeen31 <qs6272527@gmail.com>
2022-09-10 16:32:52 +0100

    first commit

--- index.py
+++ index.py
@@ -17,7 +17,7 @@ app.layout = html.Div([
 
     html.Div([
         dcc.Interval(id = 'update_date_time_value',
-                     interval = 65000,
+                     interval = 70000,
                      n_intervals = 0),
     ]),
 
