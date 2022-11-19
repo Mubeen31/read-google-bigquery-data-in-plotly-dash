9ea36ff318a3fe364ecc3e8c4621bd85a29670de
Mubeen31 <qs6272527@gmail.com>
2022-11-19 09:25:20 +0000

    first commit

--- insert_remaining_data.py
+++ insert_remaining_data.py
@@ -2,16 +2,17 @@ from google.oauth2 import service_account
 import pandas as pd1
 import pandas_gbq as pd2
 
-header_list = ['DateTime', 'Voltage', 'ValueCurrent']
+# header_list = ['DateTime', 'Voltage', 'ValueCurrent']
 df = pd1.read_csv(
     'D:\solar energy monitoring and prediction system\solar power and weather data/sensors_data.csv',
-    names = header_list)
-df4 = df[(df['DateTime'] >= '2022-10-01 00:00:39') & (df['DateTime'] <= '2022-10-01 23:59:56')][
-    ['DateTime', 'Voltage', 'ValueCurrent']]
-credentials = service_account.Credentials.from_service_account_file('solardata-key.json')
-project_id = 'solardata-360222'
-table_id = 'solardata-360222.SolarSensorsData.SensorsData'
-df5 = pd2.to_gbq(df4,
+    # names = header_list
+)
+# df4 = df[(df['DateTime'] >= '2022-10-04 00:13:11') & (df['DateTime'] <= '2022-10-04 11:55:42')][
+#     ['DateTime', 'Voltage', 'ValueCurrent']]
+credentials = service_account.Credentials.from_service_account_file('data-streaming-key.json')
+project_id = 'data-streaming-368616'
+table_id = 'data-streaming-368616.DataStreamingSolarData.SolarSensorsData'
+df5 = pd2.to_gbq(df,
                  destination_table = table_id,
                  project_id = project_id,
                  credentials = credentials,
