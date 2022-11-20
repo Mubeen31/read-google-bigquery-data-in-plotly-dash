9ba3a550150f01fc5034878d9391435a6e11f581
Mubeen Ali <qs6272527@gmail.com>
2022-11-20 10:14:41 +0000

    first commit

--- insert_remaining_data.py
+++ insert_remaining_data.py
@@ -4,14 +4,14 @@ import pandas_gbq as pd2
 
 # header_list = ['DateTime', 'Voltage', 'ValueCurrent']
 df = pd1.read_csv(
-    'D:\solar energy monitoring and prediction system\solar power and weather data/sensors_data.csv',
+    'sensors_data.csv',
     # names = header_list
 )
 # df4 = df[(df['DateTime'] >= '2022-10-04 00:13:11') & (df['DateTime'] <= '2022-10-04 11:55:42')][
 #     ['DateTime', 'Voltage', 'ValueCurrent']]
 credentials = service_account.Credentials.from_service_account_file('data-streaming-key.json')
-project_id = 'data-streaming-368616'
-table_id = 'data-streaming-368616.DataStreamingSolarData.SolarSensorsData'
+project_id = 'type here project id'
+table_id = 'type here table id'
 df5 = pd2.to_gbq(df,
                  destination_table = table_id,
                  project_id = project_id,
