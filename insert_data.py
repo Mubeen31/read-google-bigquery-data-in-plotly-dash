a567eb43aaa3c3aed4a4b5bd0eb2b20b20a85c10
Mubeen Ali <qs6272527@gmail.com>
2022-11-20 10:12:22 +0000

    first commit

--- insert_data.py
+++ insert_data.py
@@ -6,17 +6,15 @@ import time
 while True:
     time.sleep(62)
     header_list = ['DateTime', 'Voltage', 'Current']
-    df = pd.read_csv(
-        'C:/Users/Lenovo Owner/Documents/solar power and wind power predictions/solar-power-and-weather-data/sensors_data.csv',
-        names=header_list)
+    df = pd.read_csv('sensors_data.csv', names=header_list)
     data_time = df['DateTime'].tail(1).iloc[0]
     voltage = df['Voltage'].tail(1).iloc[0]
     current = df['Current'].tail(1).iloc[0]
 
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'solardata-key.json'
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'data-streaming-key.json'
 
     client = bigquery.Client()
-    table_id = 'solardata-360222.SolarSensorsData.SensorsData'
+    table_id = 'type here table id'
 
     rows_to_insert = [
         {u'DateTime': data_time, u'Voltage': voltage, u'ValueCurrent': current}
